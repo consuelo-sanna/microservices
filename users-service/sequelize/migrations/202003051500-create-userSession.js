74e0f3931a1840445ef791fbfefe2d76e8ad6718
Consuelo Sanna <consu.sanna@gmail.com>
2020-03-05 15:18:12 +0100

    users db added

---
+++ users-service/sequelize/migrations/202003051500-create-userSession.js
@@ -0,0 +1,42 @@
+//crea la tabella e spiega il tipo di dati di ogni campo
+
+module.exports.up = (queryInterface, DataTypes) => {
+    return queryInterface.createTable(
+        'userSession',
+        {
+            id: {
+                allowNull: false,
+                primaryKey: true,
+                type: DataTypes.UUID,
+            },
+            userId: {
+                allowNull: false,
+                references: {
+                    key: 'id',
+                    model: 'users',
+                },
+                type: DataTypes.UUID,
+            },
+            email: {
+                allowNull: false,
+                type: DataTypes.STRING,
+                unique: true,
+            },
+            expiresAt: {
+                allowNull: false,
+                type: DataTypes.DATE,
+            },
+
+            createdAt: {
+                allowNull: false,
+                type: DataTypes.DATE,
+            },
+        },
+        {
+            charset: 'utf8',
+        }
+    );
+};
+
+module.exports.down = queryInterface =>
+    queryInterface.dropTable('userSessions');
