dbeac222f9949b6363841d008abc22866d2204ab
Consuelo Sanna <consu.sanna@gmail.com>
2020-03-06 14:44:19 +0100

    gateway, its routes and graph

---
+++ api-gateway/src/graphql/resolvers/Query/listings.js
@@ -0,0 +1,7 @@
+//import ListingsService from '#root/adapters/ListingsService';
+
+const listingsResolver = async () => {
+    [{ description: 'oaiwdjoaiwd', id: 2, title: 'mock' }];
+};
+
+export default listingsResolver;
